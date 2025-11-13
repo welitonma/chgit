270f3f8d9fe81666ce347a1b9bcf85fb5af80fec
Weliton Miranda <welitonma@live.com>
2025-11-12 23:16:15 -0300

    Delete dist directory

--- dist/github/github.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class GithubService {
-    private octokit;
-    constructor();
-    criarIssue(owner: string, repo: string, title: string, body: string, projectId?: string): Promise<Record<string, any>>;
-    listarProjetosV2(login: string): Promise<any>;
-    detalhesProjetoV2(projectId: string): Promise<any>;
-    getRateLimit(): Promise<any>;
-}
